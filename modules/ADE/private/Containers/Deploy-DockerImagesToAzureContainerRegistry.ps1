47e4c218bd80f3b5dbd77783e9ca26a525865538
Brandon Martinez <me@brandonmartinez.com>
2020-10-23 10:38:15 -0400

    Updated Deploy script to remove the removal step of docker images

--- modules/ADE/private/Containers/Deploy-DockerImagesToAzureContainerRegistry.ps1
+++ modules/ADE/private/Containers/Deploy-DockerImagesToAzureContainerRegistry.ps1
@@ -49,29 +49,6 @@ function Deploy-DockerImagesToAzureContainerRegistry {
     
     docker push "$containerRegistryLoginServer/azure-powershell"
     Confirm-LastExitCode
-    
-    Write-Log "Removing local docker images"
-    docker rmi microsoft/azure-cli
-    Confirm-LastExitCode
-    
-    docker rmi mysql:latest
-    Confirm-LastExitCode
-    
-    docker rmi wordpress:latest
-    Confirm-LastExitCode
-    
-    docker rmi mcr.microsoft.com/azure-powershell
-    Confirm-LastExitCode
-    
-    docker rmi "$containerRegistryLoginServer/azure-cli:latest"
-    Confirm-LastExitCode
-    
-    docker rmi "$containerRegistryLoginServer/mysql:latest"
-    Confirm-LastExitCode
-    
-    docker rmi "$containerRegistryLoginServer/wordpress:latest"
-    Confirm-LastExitCode
-    
-    docker rmi "$containerRegistryLoginServer/azure-powershell"
-    Confirm-LastExitCode
+
+    Write-ScriptSection "Finished Azure Container Registry Docker Images Deployment"
 }
