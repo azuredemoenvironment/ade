6eb6767a1e8ee8e19b800f3cc8097199524bd618
Brandon Martinez <me@brandonmartinez.com>
2020-12-08 09:27:32 -0500

    Disabling cluster auto scaler (and re-enabling when needed). Fixes #53

--- modules/ADE/private/Containers/Set-AzureKubernetesServicesClusterToStopped.ps1
+++ modules/ADE/private/Containers/Set-AzureKubernetesServicesClusterToStopped.ps1
@@ -5,6 +5,9 @@ function Set-AzureKubernetesServicesClusterToStopped {
 
     Write-ScriptSection "Setting Azure Kubernetes Services Cluster to Stopped (for Cost Savings)"
     
+    az aks update -g $armParameters.aksResourceGroupName -n $armParameters.aksClusterName --disable-cluster-autoscaler
+    Confirm-LastExitCode
+
     az aks stop -g $armParameters.aksResourceGroupName -n $armParameters.aksClusterName
     Confirm-LastExitCode
     
