fb32027057cbfc9126ab15f61d34c0ee4b4796ba
Brandon Martinez <me@brandonmartinez.com>
2021-04-07 12:39:12 -0400

    Refined subscription picker, added in get-access-token to auto-login to powershell

--- login.ps1
+++ login.ps1
@@ -16,54 +16,56 @@ function Write-Header {
 
 Write-Header "Logging in to az CLI"
 
-if ($subscriptionName -eq $null -or $subscriptionName -eq "")
-{
-    ## get all subscriptions and save into variable
-    $subscriptions=$(az account list --query "[].{Name:name,subscriptionId:id}" --output json)
+az login
 
-    ## get a count of subscriptions for loop
-    [int]$subscriptionCount = ($subscriptions | ConvertFrom-Json).count
+if ($subscriptionName -eq $null -or $subscriptionName -eq "") {
+    # get all subscriptions and save into variable
+    $subscriptions = $(az account list --query "[].{Name:name,subscriptionId:id}" --output json) | ConvertFrom-Json
 
-    ## Convert to JSON for easier manipulation of data
-    $subscriptions = ($subscriptions | ConvertFrom-Json)
-    Write-Host "Found" $subscriptionCount "Subscriptions"
+    # get a count of subscriptions for loop
+    [int]$subscriptionCount = $subscriptions.count
+
+    Write-Header "Select a Subscription; found $subscriptionCount"
     
-    ##starting value for array for loop
+    # starting value for array for loop
     $i = 0
-    foreach ($subscription in $subscriptions)
-    {
-        ## start of menu - vlaue = 0
+    foreach ($subscription in $subscriptions) {
+        # start of menu - value = 0
         $subValue = $i
-        ## print out all subscriptions
+
+        # print out all subscriptions
         Write-Host $subValue ":" $subscription.Name "("$subscription.SubscriptionId")"
-        ## increment value
+
+        # increment value
         $i++
     }
-    Do 
-    {
-        ## repeat loop until valid number is chosen
+
+    Do {
+        # repeat loop until valid number is chosen
         [int]$subscriptionChoice = read-host -prompt "Select number & press enter"
     }
-    ## exit criteria for loop
+
+    # exit criteria for loop
     until ($subscriptionChoice -le $subscriptionCount)
 
     Write-Host "You selected" $subscriptions[$subscriptionChoice].Name
     $subscriptionName = $subscriptions[$subscriptionChoice].Name
 }
-else 
-{
-    $subscriptionName = $subscriptionName
-}
-
-az login
 
 Write-Header "Setting az CLI Subscription to $subscriptionName"
 
 az account set --subscription $subscriptionName
 
+Write-Header "Getting Access Token to Login to Powershell"
+
+$accountShowResponse = $(az account show --output json) | ConvertFrom-Json
+$accountId = $accountShowResponse.id
+$accessTokenResponse = $(az account get-access-token --output json) | ConvertFrom-Json
+$accessToken = $accessTokenResponse.accessToken
+
 Write-Header "Logging in to Az PowerShell"
 
-Connect-AzAccount -UseDeviceAuthentication
+Connect-AzAccount -AccountId $accountId -AccessToken $accessToken
 
 Write-Header "Setting Az PowerShell Subscription to $subscriptionName"
 
