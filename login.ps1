c34711cb18eda5b314702c87d6026d7da844daca
Fletcher Kelly <flkelly@microsoft.com>
2021-04-07 17:56:41 +0200

    add subscription picker option to login.ps1

--- login.ps1
+++ login.ps1
@@ -1,7 +1,7 @@
 #!/usr/bin/env pwsh
 
 param (
-    [Parameter(Position = 0, mandatory = $true)]
+    [Parameter(Position = 0, mandatory = $false)]
     [string]$subscriptionName
 )
 
@@ -15,6 +15,45 @@ function Write-Header {
 }
 
 Write-Header "Logging in to az CLI"
+
+if ($subscriptionName -eq $null -or $subscriptionName -eq "")
+{
+    ## get all subscriptions and save into variable
+    $subscriptions=$(az account list --query "[].{Name:name,subscriptionId:id}" --output json)
+
+    ## get a count of subscriptions for loop
+    [int]$subscriptionCount = ($subscriptions | ConvertFrom-Json).count
+
+    ## Convert to JSON for easier manipulation of data
+    $subscriptions = ($subscriptions | ConvertFrom-Json)
+    Write-Host "Found" $subscriptionCount "Subscriptions"
+    
+    ##starting value for array for loop
+    $i = 0
+    foreach ($subscription in $subscriptions)
+    {
+        ## start of menu - vlaue = 0
+        $subValue = $i
+        ## print out all subscriptions
+        Write-Host $subValue ":" $subscription.Name "("$subscription.SubscriptionId")"
+        ## increment value
+        $i++
+    }
+    Do 
+    {
+        ## repeat loop until valid number is chosen
+        [int]$subscriptionChoice = read-host -prompt "Select number & press enter"
+    }
+    ## exit criteria for loop
+    until ($subscriptionChoice -le $subscriptionCount)
+
+    Write-Host "You selected" $subscriptions[$subscriptionChoice].Name
+    $subscriptionName = $subscriptions[$subscriptionChoice].Name
+}
+else 
+{
+    $subscriptionName = $subscriptionName
+}
 
 az login
 
