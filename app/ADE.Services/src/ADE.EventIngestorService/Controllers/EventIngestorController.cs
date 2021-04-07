64de5c486385cddfcc132d278640fd089ab561cb
Halaa Menasy <menasyhalaa@gmail.com>
2021-04-07 15:26:46 -0400

    sending event

--- app/ADE.Services/src/ADE.EventIngestorService/Controllers/EventIngestorController.cs
+++ app/ADE.Services/src/ADE.EventIngestorService/Controllers/EventIngestorController.cs
@@ -4,22 +4,28 @@ using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ADE.DataContracts;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System.Text;
 
 namespace ADE.EventIngestorService.Controllers
 {
     [ApiController, Route("[controller]")]
     public class EventIngestorController : ControllerBase
     {
-     
-        private readonly ILogger<EventIngestorController> _logger;
 
+        private readonly ILogger<EventIngestorController> _logger;
+        private readonly ConnectionConfiguration _connectionConfiguration;
         private readonly TelemetryClient _telemetry;
 
-        public EventIngestorController(TelemetryClient telemetry, ILogger<EventIngestorController> logger)
+
+        public EventIngestorController(TelemetryClient telemetry, ILogger<EventIngestorController> logger, ConnectionConfiguration connectionConfiguration)
         {
             _telemetry = telemetry;
             _logger = logger;
+            _connectionConfiguration = connectionConfiguration;
         }
+
 
         [HttpPost]
         public async Task<FacilityEvent> PostAsync([FromBody] FacilityEvent facilityEvent)
@@ -31,9 +37,24 @@ namespace ADE.EventIngestorService.Controllers
             //_telemetry.TrackEvent("Event Ingest", facilityEvent.ToDictionary());
 
             //add code to send to event hub
-            
 
-            return facilityEvent;
+            // Create a producer client that you can use to send events to an event hub
+            await using (var producerClient = new EventHubProducerClient(_connectionConfiguration.EventHubConnectionString, _connectionConfiguration.EventHubNameSpace))
+            {
+                // Create a batch of events 
+                using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+
+                // Add events to the batch. An event is a represented by a collection of bytes and metadata. 
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Sink One")));
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Sink Two")));
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Sink Three")));
+
+                // Use the producer client to send the batch of events to the event hub
+                await producerClient.SendAsync(eventBatch);
+                Console.WriteLine("A batch of 3 events has been published.");
+
+                return facilityEvent;
+            }
         }
     }
 }
