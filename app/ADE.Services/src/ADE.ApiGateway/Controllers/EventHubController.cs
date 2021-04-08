65f910b97a1a4918f278798bef4ec1dc50423e09
chixcancode <lalovi@microsoft.com>
2021-04-08 16:09:07 -0400

    Add controller to the gateway.

---
+++ app/ADE.Services/src/ADE.ApiGateway/Controllers/EventHubController.cs
@@ -0,0 +1,34 @@
+﻿using ADE.DataContracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+using System.Threading.Tasks;
+
+namespace ADE.ApiGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventHubController : ControllerBase
+    {
+        private readonly ConnectionConfiguration _connectionConfiguration;
+
+        private readonly ILogger<EventHubController> _logger;
+
+        public EventHubController(ConnectionConfiguration connectionConfiguration, ILogger<EventHubController> logger)
+        {
+            _connectionConfiguration = connectionConfiguration;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<FacilityEvent> PostAsync([FromBody] FacilityEvent data)
+        {
+            var client = new RestClient(_connectionConfiguration.DataIngestorServiceUri);
+
+            var request = new RestRequest("eventingestor", DataFormat.Json);
+            request.AddJsonBody(data);
+
+            return await client.PostAsync<FacilityEvent>(request);
+        }
+    }
+}
