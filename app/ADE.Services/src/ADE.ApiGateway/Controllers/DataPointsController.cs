63397a0eb6907c1204e979866a1083fd9ce379cf
Halaa Menasy <menasyhalaa@gmail.com>
2021-07-08 15:01:23 -0400

    connecting to datapoints controller to see actual events

--- app/ADE.Services/src/ADE.ApiGateway/Controllers/DataPointsController.cs
+++ app/ADE.Services/src/ADE.ApiGateway/Controllers/DataPointsController.cs
@@ -40,7 +40,26 @@ namespace ADE.ApiGateway.Controllers
             var request = new RestRequest("dataingestion", DataFormat.Json);
             request.AddJsonBody(data);
 
-            return await client.PostAsync<UserDataPoint>(request);
+            var returnedData = await client.PostAsync<UserDataPoint>(request);
+
+            var eventRestClient = new RestClient(_adeConfiguration.EventIngestorServiceUri);
+
+            var eventRequest = new RestRequest("eventingestor", DataFormat.Json);
+
+            var facilityEvent = new FacilityEvent
+            {
+                EventDate = returnedData.CreatedAt,
+                FacilityId = returnedData.StringValue,
+                TenantId = returnedData.DataSource,
+                InUse = true,
+                UnitId = returnedData.UserId.ToString()
+            };
+
+            eventRequest.AddJsonBody(facilityEvent);
+
+            await eventRestClient.PostAsync<FacilityEvent>(eventRequest);
+
+            return returnedData;
         }
     }
 }
