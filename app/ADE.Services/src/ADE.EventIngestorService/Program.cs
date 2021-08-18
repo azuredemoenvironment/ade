0b1fc4117f0a04b66e4fa3b19f638f85403dc753
Brandon Martinez <me@brandonmartinez.com>
2021-08-18 16:31:41 -0400

    Added AppConfig to EventIngestorService

--- app/ADE.Services/src/ADE.EventIngestorService/Program.cs
+++ app/ADE.Services/src/ADE.EventIngestorService/Program.cs
@@ -1,26 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ADE.EventIngestorService
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureAppConfiguration(config =>
+                    {
+                        var settings = config.Build();
+                        var connection = settings.GetConnectionString("AppConfig");
+                        if(!string.IsNullOrWhiteSpace(connection))
+                        {
+                            config.AddAzureAppConfiguration(connection, true);
+                        }
+                    }).UseStartup<Startup>();
                 });
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
     }
 }
